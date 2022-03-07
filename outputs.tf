1733c8eb55dd5bbb5088af1d5b5dbd1a75e8b2a7
tim0git <tim.doolan@cdl.co.uk>
2022-03-07 16:24:32 +0000

    refactor: outputs of ssm_parameter module to be a list of parameters and their key values

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-output "ssm_parameter_name" {
-  value = aws_ssm_parameter.this.*.name
-}
-
-output "ssm_parameter_description" {
-  value = aws_ssm_parameter.this.*.description
-}
-
-output "ssm_parameter_type" {
-  value = aws_ssm_parameter.this.*.type
+output "ssm_parameters" {
+  value = [{
+    name: join("", aws_ssm_parameter.this[*].name),
+    description: join("", aws_ssm_parameter.this[*].description),
+    type: join("", aws_ssm_parameter.this[*].type),
+    version: join("", aws_ssm_parameter.this[*].version)
+    overwrite: join("", aws_ssm_parameter.this[*].overwrite)
+  }]
 }
