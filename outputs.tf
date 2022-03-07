320a1667cd156f7223dd3b917f5c8f39c087d19b
Timothy Doolan <tim0git>
2022-03-07 21:22:45 +0000

    refactor: module output to use for loop to return a list of parameter key value pairs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "ssm_parameters" {
-  value = [{
-    name: join("", aws_ssm_parameter.this[*].name),
-    description: join("", aws_ssm_parameter.this[*].description),
-    type: join("", aws_ssm_parameter.this[*].type),
-    version: join("", aws_ssm_parameter.this[*].version)
-    overwrite: join("", aws_ssm_parameter.this[*].overwrite)
-  }]
+  value = [
+    for parameter in aws_ssm_parameter.this : {
+    name = parameter.name
+    description = parameter.description
+    type = parameter.type
+    version = parameter.version
+    overwrite = parameter.overwrite
+    }
+  ]
 }
